602037ecb48855bd33ce647985dc129787d5ee89
Florent VICENTE <vicentegroupe@gmail.com>
2019-04-10 09:12:38 +0200

    Controllers1

---
+++ app.js
@@ -0,0 +1,35 @@
+// Storage Controller
+
+
+
+// Item Controller
+const ItemCtrl = (function(){
+    // Item Constructor
+    const Item = function(id, name, calories){
+        this.id = id;
+        this.name = name;
+        this.calories = calories;
+    }
+
+    // Data Structure / State
+    const data = {
+        items: [
+            {id: 0, name: 'Steak Dinner', calories: 1200},
+            {id: 0, name: 'Steak Dinner', calories: 1200},
+            {id: 0, name: 'Steak Dinner', calories: 1200}
+        ],
+        currentItem: null,
+        totalCalories: 0
+    }
+
+})();
+
+// UI Controller
+const UICtrl = (function(){
+
+})();
+
+// App Controller
+const AppCtrl = (function(ItemCtrl, UICtrl){
+
+})(ItemCtrl, UICtrl);
