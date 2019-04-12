584d5bab9d1a3672ad0c875dcc434756d9568d6f
Florent VICENTE <vicentegroupe@gmail.com>
2019-04-12 14:50:23 +0200

    FinUpdatingItems

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+// cd Desktop/Flo/CODE/Udemy/Brad Traversy Modern JavaScript/Cours/Section 12 Tracalorie Project
+
+
 // Storage Controller
-
-
 // Item Controller
 const ItemCtrl = (function(){
     // Item Constructor
@@ -56,6 +57,20 @@ const ItemCtrl = (function(){
         });
         return found;
       },
+      updateItem: function(name, calories){
+        // Calories to number
+        calories = parseInt(calories);
+        let found = null;
+        data.items.forEach(function(item){
+          if(item.id === data.currentItem.id){
+            item.name = name;
+            item.calories = calories;
+            found = item;
+          }
+        });
+
+        return found;
+      },
       setCurrentItem: function(item){
         data.currentItem = item;
       },
@@ -88,6 +103,7 @@ const ItemCtrl = (function(){
   const UICtrl = (function(){
     const UISelectors = {
       itemList: '#item-list',
+      listItems: '#item-list li',
       addBtn: '.add-btn',
       itemNameInput: '#item-name',
       itemCaloriesInput: '#item-calories',
@@ -137,6 +153,23 @@ const ItemCtrl = (function(){
         // Insert item
         document.querySelector(UISelectors.itemList).insertAdjacentElement('beforeend', li)
       },
+      updateListItem: function(item){
+        let listItems = document.querySelectorAll(UISelectors.listItems);
+
+        // Turn Node list into array
+        listItems = Array.from(listItems);
+
+        listItems.forEach(function(listItem){
+          const itemID = listItem.getAttribute('id');
+
+          if(itemID === `item-${item.id}`){
+            document.querySelector(`#${itemID}`).innerHTML = `<strong>${item.name}: </strong> <em>${item.calories} Calories</em>
+            <a href="#" class="secondary-content">
+              <i class="edit-item fa fa-pencil"></i>
+            </a>`;
+          }
+        });
+      },
       clearInput: function(){
         document.querySelector(UISelectors.itemNameInput).value = '';
         document.querySelector(UISelectors.itemCaloriesInput).value = '';
@@ -183,9 +216,19 @@ const ItemCtrl = (function(){
       // Add item event
       document.querySelector(UISelectors.addBtn).addEventListener('click', itemAddSubmit);
 
-      // Edit icon click event
-      document.querySelector(UISelectors.itemList).addEventListener('click', itemUpdateSubmit);
+      // Disable submit on enter
+      document.addEventListener('keypress', function(e){
+        if(e.keyCode === 13 || e.which === 13){
+          e.preventDefault();
+          return false;
+        }
+      });
 
+      // Edit icon click event
+      document.querySelector(UISelectors.itemList).addEventListener('click', itemEditClick);
+
+      // Update item event
+      document.querySelector(UISelectors.updateBtn).addEventListener('click', itemUpdateSubmit);
     }
   
     // Add item submit
@@ -203,6 +246,7 @@ const ItemCtrl = (function(){
 
         // Get total calories
         const totalCalories = ItemCtrl.getTotalCalories();
+
         // Add total calories to UI
         UICtrl.showTotalCalories(totalCalories);
   
@@ -213,8 +257,8 @@ const ItemCtrl = (function(){
       e.preventDefault();
     }
 
-    // Update item submit
-    const itemUpdateSubmit = function(e){
+    // Click edit item
+    const itemEditClick = function(e){
         if(e.target.classList.contains('edit-item')){
             // Get list item id
             const listId = e.target.parentNode.parentNode.id;
@@ -236,6 +280,29 @@ const ItemCtrl = (function(){
 
         }
         e.preventDefault();
+    }
+
+    // Update item submit
+    const itemUpdateSubmit = function(e){
+      // Get item input
+      const input = UICtrl.getItemInput();
+
+      // Update item
+      const updatedItem = ItemCtrl.updateItem(input.name, input.calories);
+
+      // Update UI
+      UICtrl.updateListItem(updatedItem);
+
+      // Get total calories
+      const totalCalories = ItemCtrl.getTotalCalories();
+
+      // Add total calories to UI
+      UICtrl.showTotalCalories(totalCalories);
+
+      // Clear UI
+      UICtrl.clearEditState();
+
+      e.preventDefault();
     }
   
     // Public methods
